02241f6ba77e685359ec46cfa3ecac5f71d79f17
Shintaro Matsushima <shintaromatsushima@gmail.com>
2017-06-26 11:56:15 +0900

    Added a quick check test

--- project.clj
+++ project.clj
@@ -5,5 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/tools.cli "0.3.5"]]
+
+  ;; Leiningen test hooks clash with test.check 0.9.0
+  ;; https://github.com/technomancy/leiningen/issues/2173
+  :monkeypatch-clojure-test false
+
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :main caclj.core)
--- test/caclj/cellular_automata_test.clj
+++ test/caclj/cellular_automata_test.clj
@@ -1,5 +1,6 @@
 (ns caclj.cellular-automata-test
   (:require [clojure.test :refer :all]
+            [clojure.spec.test.alpha :as stest]
             [caclj.cellular-automata :refer :all]))
 
 (deftest init-rule-test
@@ -10,7 +11,17 @@
   (is (= (init-rule 30)
          [false true true true true false false false]))
   (is (= (init-rule 255)
-         [true true true true true true true true])))
+         [true true true true true true true true]))
+
+  (is (thrown? AssertionError (init-rule -1)))
+  (is (thrown? AssertionError (init-rule 256))))
+
+(deftest init-rule-stest
+   (is (true?
+        (-> (stest/check `init-rule)
+            first
+            :clojure.spec.test.check/ret
+            :result))))
 
 (deftest next-gen-test
   (is (= (next-gen (init-rule 30) [true])
